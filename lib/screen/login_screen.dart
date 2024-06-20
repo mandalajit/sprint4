11cbcad8b036d4928ace927d35da03cdd0980d72
mandalajit <mandalajit7878@gmail.com>
2024-06-20 23:27:29 +0545

    make register view

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:recipe/screen/dashboard_screen.dart';
 import 'package:recipe/screen/register_screen.dart';
